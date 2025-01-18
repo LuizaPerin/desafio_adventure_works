78934e9cfbb624c6030fdf761fd147073d248101
Luiza Perin <lurperin@gmail.com>
2025-01-18 18:40:43 -0300

    fix typo

--- models/staging/person/stg__state_province.sql
+++ models/staging/person/stg__state_province.sql
@@ -9,7 +9,7 @@ with
         select
             stateprovinceid as state_province_id
             , stateprovincecode as state_province_code
-            , countryregioncode as coutry_region_code
+            , countryregioncode as country_region_code
             , isonlystateprovinceflag as is_only_state_province_flag
             , name as state_province_name
             , territoryid as territory_id
